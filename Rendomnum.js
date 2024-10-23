02fe0ae01f12b6762775e510654068be7854792e
ItsForYou1 <reuse.itsforyou@gmail.com>
2024-10-23 14:08:16 +0200

    Add files via upload

---
+++ Rendomnum.js
@@ -0,0 +1,10 @@
+const mybutton = document.getElementById("mybutton");
+const label = document.getElementById("Mylabel");
+const min = 1;
+const max = 100;
+let rendomNum;
+
+mybutton.onclick = function(){
+  rendomNum = Math.floor(Math.random() * max) + min;
+Mylabel.textContent = rendomNum;
+}   
--- pg2.js
+++ pg2.js
@@ -1,11 +1,38 @@
-const mybutton = document.getElementById("mybutton");
-const label = document.getElementById("Mylabel");
-const min = 1;
-const max = 100;
-let rendomNum;
 
-mybutton.onclick = function(){
-    rendomNum = Math.floor(Math.random() * max) + min;
-    Mylabel.textContent = rendomNum;
-}   
 
+
+
+const Chosices = ["Steen", "Papier", "Schaar"];
+const PlayerDisplay = document.getElementById("PlayerDisplay");
+const ComputerDisplay = document.getElementById("ComputerDisplay");
+const ResultDisplay = document.getElementById("ResultDisplay");
+
+function PlayGame(PlayerChoices){
+
+    const ComputerChoices = Chosices[Math.floor(Math.random() * 3)];
+    let Result = "";
+
+    if (PlayerChoices === ComputerChoices) {
+        Result = "Gelijkspel!";
+    } 
+    else {
+        switch (PlayerChoices) {
+            case "Steen":
+                Result = (ComputerChoices === "Schaar") ? "Je hebt GEWONNEN!!!!" : "Je hebt verloren";
+                break;
+            case "Papier":
+                Result = (ComputerChoices === "Steen") ? "Je hebt GEWONNEN!!!!" : "Je hebt verloren";
+                break;
+            case "Schaar":
+                Result = (ComputerChoices === "Papier") ? "Je hebt GEWONNEN!!!!" : "Je hebt verloren";
+                break;
+        }
+    }
+
+    PlayerDisplay.textContent = `Speler: ${PlayerChoices}`;
+    ComputerDisplay.textContent = `Bot: ${ComputerChoices}`;
+    ResultDisplay.textContent = Result;
+
+
+    }
+    
